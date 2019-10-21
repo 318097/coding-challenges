68507d2c1f0d3888e642c3901a07384cc6b7e514
Mehul <318097@gmail.com>
2019-10-21 22:15:08 +0530

    Stack

---
+++ stack.js
@@ -0,0 +1,44 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+
+    const removed = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+
+    return removed.value;
+  }
+
+}
